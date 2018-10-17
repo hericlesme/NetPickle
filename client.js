90eb5c70e3d94a1243a125c858b4caad03b479a6
Héricles Emanuel <hericles.me@gmail.com>
2018-10-17 15:12:40 -0300

    add help command

--- client.js
+++ client.js
@@ -1,15 +1,28 @@
+/* variables */
+
 var readline = require('readline'),
-socketio = require('socket.io-client'),
-util = require('util'),
-color = require("ansi-color").set;
- 
+    socketio = require('socket.io-client'),
+    color = require("ansi-color").set;
+
 var nick;
+
+var pattern = new RegExp("[^A-Za-z]");
 var socket = socketio.connect('http://localhost:3636');
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.question("Insira seu nick: ", function(name){
-    nick = name;
-    var msg = "'"+ nick + "' entrou no mundo obscuro dos sockets... ";
+/* Prototypes */
+
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+rl.question("Insira seu nick: ", function (name) {
+
+    nick = name.replaceAll(pattern, "").toLowerCase();
+
+    var msg = "'" + nick + "' entrou no mundo obscuro dos sockets... ";
+
     socket.emit('send', {
         type: 'notice',
         message: msg
@@ -24,12 +37,12 @@ function console_out(msg) {
     rl.prompt(true);
 }
 
+
 rl.on('line', function (line) {
     if (line[0] == "/" && line.length > 1) {
         var cmd = line.match(/[a-z]+\b/)[0];
-        var arg = line.substr(cmd.length+2, line.length);
+        var arg = line.substr(cmd.length + 2, line.length);
         chat_command(cmd, arg);
- 
     } else {
         socket.emit('send', { type: 'chat', message: line, nick: nick });
         rl.prompt(true);
@@ -38,52 +51,65 @@ rl.on('line', function (line) {
 
 function chat_command(cmd, arg) {
     switch (cmd) {
- 
+        case 'help':
+            var help = " \
+            \n -- NetPickle - Comandos \
+            \n\
+            \n /nick [username] - Troca o nome de usuario \
+            \n /msg [username] [mensagem] - Envia uma mensagem privada \
+            \n /alert [mensagem] - Envia um alert para todos \
+            \n /exit - Desconecta do chat \
+            \n /help - Exibe esta mensagem \
+            \n";
+            console_out(help);
+            break;
         case 'nick':
             var notice = nick + " mudou seu nick para " + arg;
             nick = arg;
             socket.emit('send', { type: 'notice', message: notice });
             break;
- 
         case 'msg':
             var to = arg.match(/[a-z]+\b/)[0];
             var message = arg.substr(to.length, arg.length);
             socket.emit('send', { type: 'tell', message: message, to: to, from: nick });
             break;
         case 'alert':
-            socket.emit('send', {type: 'alert', message: arg});
+            socket.emit('send', { type: 'alert', message: arg });
             break;
         case 'exit':
             var exiting_message = "'" + nick + "' saiu deste mundo.";
-            socket.emit('send', {type: 'notice', message: exiting_message});
+            socket.emit('send', { type: 'notice', message: exiting_message });
             socket.disconnect();
             break;
         default:
-            console_out("Isto nao eh um comando valido.");
- 
+            console_out("Oops... parece que este nao eh um comando valido.");
+
     }
 }
-    socket.on('message', function (data) {
+
+socket.on('disconnect', (reason) => {
+    if (reason === 'io server disconnect') {
+        socket.connect();
+    }
+    socket.emit()
+    console_out('Desconectando...');
+    rl.close();
+});
+
+socket.on('message', function (data) {
     var leader;
     if (data.type == 'chat' && data.nick != nick) {
-        leader = color("[" + data.nick+"]: ", "green");
+        leader = color("[" + data.nick + "]: ", "green");
         console_out(leader + data.message);
     }
     else if (data.type == "notice") {
         console_out(color(data.message, 'cyan'));
     }
     else if (data.type == "tell" && data.to == nick) {
-        leader = color("["+data.from+"->"+data.to+"]", "red");
+        leader = color("[" + data.from + "->" + data.to + "]", "red");
         console_out(leader + data.message);
     }
     else if (data.type == "alert") {
-        console_out(color('!' + data.message + '!', "blink+black+yellow_bg"));
+        console_out(color('ALERT: ' + data.message + '!', "black+yellow_bg"));
     }
-
-    socket.on('disconnect', (reason) => {
-        if (reason === 'io server disconnect') {
-            socket.connect();
-        }
-        rl.close();
-    });
 });
